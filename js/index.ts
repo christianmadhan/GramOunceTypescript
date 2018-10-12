0c100454cccb1e8a3ad4ee35c391f701ddc24297
NORDLYS\cmiw <christianmadhan>
2018-10-12 11:20:36 +0200

    added new

--- js/index.ts
+++ js/index.ts
@@ -1,79 +1,16 @@
-import axios, {
-    AxiosResponse,
-    AxiosError
-} from "../../node_modules/axios";
+let inputOunce: HTMLInputElement = <HTMLInputElement>document.getElementById("ounce");
+let inputGram: HTMLInputElement = <HTMLInputElement>document.getElementById("gram");
 
+let convertOuncebtn:  HTMLButtonElement = <HTMLButtonElement>document.getElementById("convertToOunce");
+let convertGrambtn:  HTMLButtonElement = <HTMLButtonElement>document.getElementById("convertToGram");
 
-let getCarsbtn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("getAllButton");
+let convertedOunce: HTMLSpanElement = <HTMLSpanElement>document.getElementById("convertedOunce");
+let convertedGram: HTMLSpanElement = <HTMLSpanElement>document.getElementById("convertedGram");
 
-interface ICar {
-    model: string;
-    vendor: string;
-    price: number;
-}
+convertGrambtn.addEventListener("click", ()=> {
+        convertedOunce.innerHTML = parseFloat(inputOunce.value) * 28.35 +  "";
+});
 
-function doGetAll(): void {
-    let uri: string = "http://rest-pele-easj-dk.azurewebsites.net/api/Cars";
-    axios.get<ICar[]>(uri)
-        .then(function (response: AxiosResponse<ICar[]>): void {
-            console.log(response.data);
-            let result: string = "<ol>";
-            response.data.forEach((car: ICar) => {
-                if(car !== null){
-                    result += "<li>" + car.model + " " + car.price + "</li>";
-                }
-                 
-            })
-            result += "</ol>";
-            let allCars: HTMLDivElement = <HTMLDivElement>document.getElementById("allCars");
-            allCars.innerHTML = result;
-
-            console.log(result);
-        })
-        .catch(function (error: AxiosError) {
-            console.log(error);
-        });
-}
-let addCarBtn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("addCarButton");
-
-
-function addCar(): void {
-    let modelInput: HTMLInputElement = <HTMLInputElement>document.getElementById("addModel");
-    let vendorInput: HTMLInputElement = <HTMLInputElement>document.getElementById("addVendor");
-    let priceInput: HTMLInputElement = <HTMLInputElement>document.getElementById("addPrice");
-
-    let addModel: string = modelInput.value;
-    let addVendor: string = vendorInput.value;
-    let addPrice: number = Number(priceInput.value);
-
-    let uri: string = "http://rest-pele-easj-dk.azurewebsites.net/api/Cars"
-    axios.post<ICar>(uri, {model: addModel, vendor: addVendor, price: addPrice})
-    .then(function(response: AxiosResponse): void{
-        console.log(response.status + " " + response.statusText);
-    })
-    .catch((error: AxiosError)=>{
-        console.log(error);
-    })
-}
-
-let delbtn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("deleteCar");
-
-function deleteCar(): void {
-    let modelElement : HTMLInputElement = <HTMLInputElement>document.getElementById("deleteModel");
-    let deleteThisModel: string = modelElement.value;
-
-    let uri: string ="http://rest-pele-easj-dk.azurewebsites.net/api/Cars/" + deleteThisModel;
-
-    axios.delete(uri)
-    .then(function(response: AxiosResponse): void{
-            console.log(response);
-    })
-    . catch((error: AxiosError) => {
-        console.log(error)
-    })
-}
-
-
-delbtn.addEventListener("click", deleteCar);
-addCarBtn.addEventListener("click",addCar)
-getCarsbtn.addEventListener("click", doGetAll);
+convertOuncebtn.addEventListener("click", ()=> {
+    convertedGram.innerHTML = parseFloat(inputGram.value) / 28.35 +  " ";
+});
